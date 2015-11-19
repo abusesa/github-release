f46b0503ee5165ff34ac55b044822c23d44e5b70
Joachim Viide <jviide@iki.fi>
2015-11-19 18:59:26 +0200

    Make the script installable with pip

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="github-release",
+    version="0.0.1",
+    description="A helper tool for creating GitHub releases",
+    author="Joachim Viide <jviide@iki.fi>",
+    url="https://github.com/abusesa/github-release",
+    license="MIT",
+    scripts=[
+        "scripts/github-release"
+    ],
+    install_requires=[
+        "requests >= 2.8.1",
+        "argparse >= 1.4.0"
+    ]
+)
