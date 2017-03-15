b42d4d8a95a42b128a7d12f7d5f3fbc8a30fd42f
Mika Seppänen <mika.seppanen@iki.fi>
2017-03-15 13:58:09 +0200

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="github-release",
-    version="0.0.3",
+    version="0.0.4",
     description="A helper tool for creating GitHub releases",
     author="Joachim Viide <jviide@iki.fi>",
     url="https://github.com/abusesa/github-release",
